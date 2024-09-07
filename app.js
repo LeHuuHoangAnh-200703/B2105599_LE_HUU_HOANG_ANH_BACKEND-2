41fdcf66bb45e683bf8bf6c1d3242002bf86f6ce
LeHuuHoangAnh-200703 <lehuuhoanganhhg2003@gmail.com>
2024-09-07 16:23:46 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const cors = require("cors");
 const contactRouter = require("./app/routes/contact.route.js");
+const ApiError = require("./app/api-error.js");
 
 const app = express();
+
 app.use(cors());
-app.use(express.json())
+app.use(express.json());
+
 app.use("/api/contacts", contactRouter);
+
+app.use((req, res, next) => {
+  return next(new ApiError(404, "Resource not found"));
+});
+  
+app.use((err, req, res, next) => {
+  res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+  });
+});
 
 app.get("/", (req, res) => {
   res.json({ message: "Welcome to contact book application." });
